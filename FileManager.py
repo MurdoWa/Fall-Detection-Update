ecdd4fba41d0ce54471fdfbfa4b15fbc6e7e5a17
Murdo Wallace <40591751@live.napier.ac.uk>
2026-03-18 17:12:51 +0000

    Added pausing

--- FileManager.py
+++ FileManager.py
@@ -13,10 +13,3 @@ def RenameIMG(saveLocation, fallpred, threshold):
 
 def DeleteIMG(saveLocation, fileName):
     os.remove(saveLocation+fileName)
-
-
-
-def findNextFile(saveLocation):
-    files = [f for f in os.listdir(saveLocation) if os.path.isfile(os.path.join(saveLocation, f))]
-    numfiles = len(files)
-    return numfiles
