db1be26e6fb3982af5589f7c7cd9f794ac133b22
Murdo Wallace <40591751@live.napier.ac.uk>
2026-03-30 13:05:33 +0100

    updated RenameIMG

--- FileManager.py
+++ FileManager.py
@@ -6,9 +6,11 @@ def RenameIMG(saveLocation, fallpred, threshold):
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y-%H_%M_%S")
     if fallpred > threshold:
-        os.rename(saveLocation+"TEMPNAME.png", saveLocation+"fall_"+dt_string+".png")
+        new_name = "fall_" + dt_string + ".png"
+        os.rename(saveLocation + "TEMPNAME.png", saveLocation + new_name)
+        return new_name
 
     elif fallpred < threshold:
-        os.rename(saveLocation+"TEMPNAME.png", saveLocation+"nofall_"+dt_string+".png")
-
-
+        new_name = "nofall_" + dt_string + ".png"
+        os.rename(saveLocation + "TEMPNAME.png", saveLocation + new_name)
+        return new_name
